ea0a3bed375c363c2c4778fba2afb4c7268698af
Sear5566 <dwdsegun63@gmail.com>
2024-01-22 02:38:41 +0000

    Added Header file and bubble sort file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,45 @@
+#include "sorter.h"
+void swap(int *first, int *second);
+
+/**
+ * bubble_sort - sorting algorithm
+ * @array: data
+ * @size: size data
+ * Return: nothing.
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int swapped = 0;
+
+	if (array == NULL || size <= 1)
+		return;
+	for (i = 0; i < size - 1 ; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
+}
+
+/**
+ *swap - Exchange values
+ * @first: 1th element
+ * @second: 2th element
+ * Return: nothing
+*/
+void swap(int *first, int *second)
+{
+	int sw = *first;
+	*first = *second;
+	*second = sw;
+}
